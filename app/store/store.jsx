82dee91b8bcec9b94a5d7bbf2d6d05103a47e95f
Andrée Ray <info@andreeray.se>
2017-03-10 04:55:42 +0100

    prior bransh redux

--- app/store/store.jsx
+++ app/store/store.jsx
@@ -1,23 +1,8 @@
 /**
-* @Author: Andreee Ray <DevelDoe>
+* @Author: Andreee Ray <develdoe>
 * @Date:   2017-03-05T21:30:28+01:00
 * @Email:  me@andreeray.se
-* @Filename: config.jsx
+* @Filename: store.jsx
 * @Last modified by:   develdoe
-* @Last modified time: 2017-03-10T04:46:53+01:00
+* @Last modified time: 2017-03-10T04:55:11+01:00
 */
-
-
-
-var redux = require('redux'), thunk = require('redux-thunk').default, {printReducer} = require('./../reducers/reducers')
-
-export var config = () => {
-    var reducer = redux.combineReducers({ print: printReducer })
-
-    var store = redux.createStore(reducer, redux.compose(
-        redux.applyMiddleware(thunk),
-        window.devToolsExtension ? window.devToolsExtension() : f => f
-    ))
-
-    return store
-}
